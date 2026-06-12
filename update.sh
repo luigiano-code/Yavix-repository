2d28b56318c04042eb45033c40543bc494a1a14a
luigiano-code <luigiano16@proton.me>
2026-06-12 17:47:29 +0200

    Updated repository

--- update.sh
+++ update.sh
@@ -1,21 +1,2 @@
 #!/bin/bash
-
-sudo rm -rf /usr/bin/update-files/
-curl -O https://raw.githubusercontent.com/luigiano-code/Yavix-repository/main/update.tar.gz
-sudo mkdir /usr/bin/update-files
-sudo mv update.tar.gz /usr/bin/update-files/update.tar.gz
-
-sudo tar -xzvf /usr/bin/update-files/update.tar.gz -C /usr/bin/update-files/
-
-sudo rm -f /etc/os-release
-sudo cp -f /usr/bin/update-files/os-release /etc/os-release 
-
-sudo rm -f /etc/version.txt
-sudo cp -f /usr/bin/update-files/version.txt /etc/version.txt
-
-sudo rm -f /etc/pacman.conf
-sudo cp -f /usr/bin/update-files/pacman.conf /etc/pacman.conf
-
-sudo pacman -Sy
-
-sudo pacman -S eog gnome-text-editor gnome-calculator --noconfirm
+echo 12-08-2026
