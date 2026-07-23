232b31b66554870441349a03838721e2e9a0faa0
luigiano-code <luigiano16@proton.me>
2026-07-23 12:16:06 +0200

    New update ( 2.3 ) available

--- update.sh
+++ update.sh
@@ -3,4 +3,5 @@ sudo curl -O https://raw.githubusercontent.com/luigiano-code/Yavix-repository/ma
 sudo tar -xzf update.tar.gz
 sudo cp -rf update/etc/* /etc
 sudo cp -rf update/usr/* /usr
-
+sudo rm -rf /usr/share/yavix-themes/old
+systemctl reboot
